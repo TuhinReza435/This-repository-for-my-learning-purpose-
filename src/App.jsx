4677515e7b88f25af288b8a3f34110a9ad9def78
Tuhin Reza <tuhinreza02042002@gmail.com>
2025-12-25 14:47:50 +0600

    add my practice project with tailwind and react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PracticeWork from "./componetn/practiceWork";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <PracticeWork />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componetn/practiceWork.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const practice_work = () => {
+    return (
+      <div>
+        <div className='my-task'>We are learning React from tapaScript</div>
+      </div>
+    );
+};
+
+export default practice_work;
