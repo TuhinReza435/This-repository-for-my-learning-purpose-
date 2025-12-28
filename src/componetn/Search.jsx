67cd5d3066814102d3d5f68716ce5972e3932ef5
Tuhin Reza <tuhinreza02042002@gmail.com>
2025-12-28 15:53:43 +0600

    done

--- src/componetn/Search.jsx
+++ src/componetn/Search.jsx
@@ -1,7 +1,12 @@
 import Box from "./Box";
 import Premium from "./premium";
 
-export default function Search({ searchItem, onsetSearchItem, isChacked, onsetIsChacked }) {
+export default function Search({
+  searchItem,
+  onsetSearchItem,
+  isChacked,
+  onsetIsChacked,
+}) {
   return (
     <div className="flex justify-start gap-7">
       <Box searchItem={searchItem} onsetSearchItem={onsetSearchItem} />
